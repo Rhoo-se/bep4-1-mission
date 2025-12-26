723b2f4ff63eb0d7ac36c4a7d20b829f72435c60
Rho <tmddjr_0331@naver.com>
2025-12-26 12:21:12 +0900

    0024

--- src/main/java/com/back/global/jpa/entity/BaseManualIdAndTime.java
+++ src/main/java/com/back/global/jpa/entity/BaseManualIdAndTime.java
@@ -1,27 +1,29 @@
 package com.back.global.jpa.entity;
 
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
 @Getter
-public abstract class BaseIdAndTime extends BaseEntity {
+public abstract class BaseManualIdAndTime extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = IDENTITY)
     private int id;
     @CreatedDate
     private LocalDateTime createDate;
     @LastModifiedDate
     private LocalDateTime modifyDate;
+
+    public BaseManualIdAndTime(int id) {
+        this.id = id;
+    }
 }
