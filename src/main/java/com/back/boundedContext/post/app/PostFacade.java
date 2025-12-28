6e5331d3a80c337d5f3ea30fd742eac322565827
Rho <tmddjr_0331@naver.com>
2025-12-28 23:48:06 +0900

    0021

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -29,7 +29,7 @@ public class PostFacade {
     }
 
     @Transactional
-    public RsData<Post> write(Member author, String title, String content) {
+    public RsData<Post> write(PostMember author, String title, String content) {
         return postWriteUseCase.write(author, title, content);
     }
 
@@ -38,6 +38,7 @@ public class PostFacade {
         return postRepository.findById(id);
     }
 
+    @Transactional
     public PostMember syncMember(MemberDto member){
         PostMember postMember = new PostMember(
                 member.getId(),
@@ -50,5 +51,10 @@ public class PostFacade {
         );
 
         return postMemberRepository.save(postMember);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<PostMember> findPostMemberByUsername(String username) {
+        return postMemberRepository.findByUsername(username);
     }
 }
