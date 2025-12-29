af58efa0458280ff1638287287ea29d6f01cf631
Rho <tmddjr_0331@naver.com>
2025-12-29 10:18:57 +0900

    0031:

--- src/main/java/com/back/boundedContext/market/out/MarketMemberRepository.java
+++ src/main/java/com/back/boundedContext/market/out/MarketMemberRepository.java
@@ -1,7 +1,12 @@
 package com.back.boundedContext.market.out;
 
 import com.back.boundedContext.market.domain.MarketMember;
+import com.back.boundedContext.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface MarketMemberRepository extends JpaRepository<MarketMember, Integer> {
 }
